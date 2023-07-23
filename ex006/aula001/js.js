04a08764959ada32c5bf9f76ed61cddc407cf349
Wesley <wesley-gustavo@hotmail.com>
2023-07-23 10:51:59 -0300

    ex

---
+++ ex006/aula001/js.js
@@ -0,0 +1,35 @@
+function contar (){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById ('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
+        window.alert (' [ERRO] Faltam dados!.')
+        res.innerHTML = ' Impossivel Contar !.'
+    }else {
+        res.innerHTML = 'Contando:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo Inválido!')
+        }
+        if ( i< f){
+            for(let c = i; c<= f; c+= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        
+
+        }else {
+            for( let c =i; c>= f; c -=p ){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += '\u{1F3C1}'
+
+        
+    }
+}
+
+    
